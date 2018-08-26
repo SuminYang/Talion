410d11ca420051c7fe0af6f6b7bad7f376b58c4f
park <wer3799@naver.com>
2018-08-26 23:52:27 +0900

    인벤토리 작업중

---
+++ Assets/ItemBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    private ItemType type;
+    public void Initialize()
+    {
+
+    }
+    public void GetItem()
+    {
+
+    }
+}
